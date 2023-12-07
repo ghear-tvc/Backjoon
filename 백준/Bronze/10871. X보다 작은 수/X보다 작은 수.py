a041938a2f1413ccbef0c33866c72f5e312ea892
ghear-tvc <153110819+ghear-tvc@users.noreply.github.com>
2023-12-07 12:38:27 +0900

    [Bronze V] Title: X보다 작은 수, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,9 @@
-import random
 n, x = map(int, input().split())
 a =list( map(int, input().split()))
 for i in range(n) :
     if a[i] < x :
         print(a[i], end=' ')
+
+# short code
+# n,x=map(int,input().split())
+# print(' '.join(i for i in input().split() if int(i)<x))
