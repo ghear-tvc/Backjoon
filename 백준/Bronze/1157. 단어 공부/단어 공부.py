2c22651de6eab2f6ac598244755d85f7dd036413
ghear-tvc <153110819+ghear-tvc@users.noreply.github.com>
2023-12-09 00:50:03 +0900

    [Bronze I] Title: 단어 공부, Time: 96 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,16 +1,9 @@
-word = input()
-n = list(set(word.upper())) #set : 중복제거 및 순서랜덤
-num = []
-for i in n :
-    num.append(word.upper().count(i))
-max = max(num)
-cnt = 0
-ind = 0
-for i in range(len(num)) :
-   if  num[i] == max :
-       cnt += 1
-       ind = i
-if cnt == 1 :
-    print(n[ind])
-else :
-    print('?')
+word = input().upper()
+cnt =0
+for i in set(word) :
+    if word.count(i) > cnt :
+        cnt = word.count(i)
+        d = i
+    elif word.count(i) == cnt:
+        d = '?'
+print(d)
